3b0b07b8dfc23445cbc0b587043f1ab5038ae609
Sona0014 <soniyagurjar6666@gmail>
2023-07-31 10:46:29 +0530

    ghhg

--- controllers/admin/BookController.js
+++ controllers/admin/BookController.js
@@ -1,9 +1,11 @@
 const BookModel = require('../../models/Book')
+const nodemailer =require('nodemailer')
 
 class BookController {
     static bookinsert = async (req, res) => {
         try {
-            console.log(req.body);
+            //console.log(req.body);
+            const{name,email} =req.body
             const result = new BookModel({
                 name: req.body.name,
                 email: req.body.email,
@@ -11,6 +13,7 @@ class BookController {
                 address: req.body.address
             })
             await result.save()
+            this.sendEmail(name,email)
             res.redirect('/admin/book')
         } catch (error) {
             console.log(error);
@@ -28,6 +31,31 @@ class BookController {
             console.log(error);
         }
     }
+    static sendEmail = async (name, email) => {
+        //console.log("email sending")
+        //console.log("schoolName")
+        //console.log(name,email)
+
+        //connect with the smtp server
+
+        let transporter = await nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+
+            auth: {
+                user: "soniyagurjar6666@gmail.com",
+                pass: "dscrmqayredqoili",
+            },
+        });
+        let info = await transporter.sendMail({
+            from: "test@gmail.com", //sender address
+            to: email, //list of recievers
+            subject: "create School Registration Successfully", //subject line
+            text: "hello", // plain text body
+            html: `<b>${name}</b> Book School is Successful!`, //html body
+        });
+        console.log("Message sent: %s", info.messageId);
+    };
    
     
 
--- controllers/admin/ContactController.js
+++ controllers/admin/ContactController.js
@@ -1,4 +1,4 @@
-const ContactModel = require("../../models/contact");
+const ContactModel = require("../../models/Contact");
 
 class ContactController{
 
